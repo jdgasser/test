50c5034c6e96ed3f13c1d3ca019670dc1ddca921
jdgasser <35599913+jdgasser@users.noreply.github.com>
2018-01-31 14:43:49 +0100

    1st

---
+++ install.sh
@@ -0,0 +1,45 @@
+﻿	
+#mise en place du root http	
+mkdir /home/server_suivi
+rm -rf /var/www/html
+ln -sf /home/server_suivi/app-etudiant/dist /var/www/html
+ln -sf /etc/apache2/sites-available/vhost_backend.conf /etc/apache2/sites-enabled/vhost_backend.conf
+service apache2 restart
+
+
+# pour passer en dev :
+#git branch DEV
+#git checkout DEV
+
+#modif config du site
+sed -i "s/$password = \"\"/$password = \"root\"/g" /home/server_suivi/studentApi/config/database.php
+cd /home/server_suivi/app-etudiant
+sed -i "s/localhost\:8080/10.1.152.211\:15808/g" /home/server_suivi/app-etudiant/src/environments/environment.ts
+sed -i "s/localhost\:8080/10.1.152.211\:15808/g" /home/server_suivi/app-etudiant/src/environments/environment.prod.ts
+npm install rxjs 
+npm install zone.js
+npm install -g @angular/core --unsafe
+npm install angular2-collapsible
+npm install -g @angular/cli --unsafe
+npm install
+ng build --target=production --environment=prod
+
+#parametrage mysql et chargement des données
+mysqladmin password root
+mysql -uroot -proot < /home/server_suivi/sql/DBSuiviEtudiant.sql
+mysql -uroot -proot students_db< /home/server_suivi/sql/Data.sql
+sed -i "s/bind-address/\#bind-address/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+service mysql restart
+
+#installation silencieuse de phpmyadmin
+APP_PASS="root"
+ROOT_PASS="root"
+APP_DB_PASS="root"
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $ROOT_PASS" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_DB_PASS" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+apt-get install -y phpmyadmin
+service apache2 restart
+
---
+++ script.sh
@@ -0,0 +1,15 @@
+﻿
+#installation des clés publiques/privées
+eval `ssh-agent -s`
+chmod 0600 /root/key_rsa
+ssh-add /root/key_rsa
+rm -rf /root/key_rsa
+	
+#Git en master
+cd /home/server_suivi
+git init
+git pull ssh://git@10.1.152.219:10022/InformationSystemNetworkandCloud/Network-system/Suivi_etudiant/suivi_etudiant.git
+
+sh /home/server_suivi/install/install.sh
+
+rm -rf /root/script.sh
