a72f43af27a95d971c9c4aad01379ce131ac34a1
jdgasser <35599913+jdgasser@users.noreply.github.com>
2018-02-16 09:36:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,15 +33,5 @@ mysqladmin password root
 sed -i "s/bind-address/\#bind-address/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql restart
 
-#installation silencieuse de phpmyadmin
-APP_PASS="root"
-ROOT_PASS="root"
-APP_DB_PASS="root"
-echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/admin-pass password $ROOT_PASS" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_DB_PASS" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
-#apt-get install -y phpmyadmin
-service apache2 restart
+
 
